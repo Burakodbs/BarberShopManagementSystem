e1e407082112c9eb1816cb372672187b206b9eba
Burak Odabaş <burak.odabas2@ogr.sakarya.edu.tr>
2024-12-13 14:08:35 +0300

    ServiceCompleted

--- Controllers/SalonController.cs
+++ Controllers/SalonController.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Rendering;
+﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using BarberShopManagementSystem.Data;
 using BarberShopManagementSystem.Models;
@@ -13,19 +8,16 @@ namespace BarberShopManagementSystem.Controllers
     public class SalonController : Controller
     {
         private readonly ApplicationDbContext _context;
-
         public SalonController(ApplicationDbContext context)
         {
             _context = context;
         }
 
-        // GET: Salon
         public async Task<IActionResult> Index()
         {
             return View(await _context.Salons.ToListAsync());
         }
 
-        // GET: Salon/Details/5
         public async Task<IActionResult> Details(int? id)
         {
             if (id == null)
@@ -44,7 +36,6 @@ namespace BarberShopManagementSystem.Controllers
             return View(salon);
         }
 
-        // GET: Salon/Create
         public IActionResult Create()
         {
             ViewBag.ServiceList = _context.Services.Select(s => new
@@ -55,9 +46,6 @@ namespace BarberShopManagementSystem.Controllers
             return View();
         }
 
-        // POST: Salon/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Create(Salon salon, int[] selectedServices)
@@ -85,7 +73,7 @@ namespace BarberShopManagementSystem.Controllers
             }).ToList();
             return View(salon);
         }
-        // GET: Salon/Delete/5
+
         public async Task<IActionResult> Delete(int? id)
         {
             if (id == null)
@@ -103,7 +91,6 @@ namespace BarberShopManagementSystem.Controllers
             return View(salon);
         }
 
-        // POST: Salon/Delete/5
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> DeleteConfirmed(int id)
--- Controllers/ServiceController.cs
+++ Controllers/ServiceController.cs
@@ -1,8 +1,12 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
 using BarberShopManagementSystem.Data;
 using BarberShopManagementSystem.Models;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 
 namespace BarberShopManagementSystem.Controllers
 {
@@ -15,6 +19,85 @@ namespace BarberShopManagementSystem.Controllers
             _context = context;
         }
 
-        
+        // GET: Service
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Services.ToListAsync());
+        }
+
+        // GET: Service/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var service = await _context.Services
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (service == null)
+            {
+                return NotFound();
+            }
+
+            return View(service);
+        }
+
+        // GET: Service/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Name,Price,Duration")] Service service)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(service);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(service);
+        }
+
+        // GET: Service/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var service = await _context.Services
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (service == null)
+            {
+                return NotFound();
+            }
+
+            return View(service);
+        }
+
+        // POST: Service/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var service = await _context.Services.FindAsync(id);
+            if (service != null)
+            {
+                _context.Services.Remove(service);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ServiceExists(int id)
+        {
+            return _context.Services.Any(e => e.Id == id);
+        }
     }
 }
